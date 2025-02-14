21a2e9c1d06c2dc4ceb2a7fa9d95a6256afafccc
wmb1207 <wmb1207@proton.me>
2025-02-14 11:23:26 -0300

    update

--- src/Console/Commands/UserCommand.php
+++ src/Console/Commands/UserCommand.php
@@ -31,6 +31,7 @@ class UserCommand extends Command
         $roles = explode(',' , $this->option('roles'));
         $dbRoles = Role::where('name', 'in', '$roles')->get()
           ->toArray();
+        $this-info($dbRoles);
         $id = intval($this->argument('userid'));
         $user = User::findOrFail($id);
         $this->info("Updating user: {$user->id}");
