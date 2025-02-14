216f2a7ca8113b73e04612f93a4f1573ac2368c2
wmb1207 <wmb1207@proton.me>
2025-02-14 11:19:03 -0300

    update

--- src/Console/Commands/UserCommand.php
+++ src/Console/Commands/UserCommand.php
@@ -34,6 +34,6 @@ class UserCommand extends Command
         $id = intval($this->argument('userid'));
         $user = User::findOrFail($id);
         $this->info("Updating user: {$user->id}");
-        $user->roles()->sync(array_map(fn($role) => $role->id, $dbRoles->));
+        $user->roles()->sync(array_map(fn($role) => $role->id, $dbRoles));
     }
 }
